3011a950b7b0e11653e0c7724db62962e90a24e2
Frank Wong <frankwong1213@163.com>
2014-11-04 15:32:06 +0800

    Update mysql_auto_backup.sh

---
+++ mysql_auto_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+DUMP=/usr/bin/mysqldump
+OUT_DIR=/home/mysql_data_backup_path
+LINUX_USER=root
+DB_NAME=test
+DB_USER=root
+DB_PASS=123456
+cd $OUT_DIR
+DATE=`date +%Y%m%d`
+OUT_SQL="$DATE.sql"
+TAR_SQL="data_bak_$DATE.tar.gz"
+$DUMP -u $DB_USER -p$DB_PASS $DB_NAME --default-character-set=utf8 --opt -Q -R --skip-lock-tables > $OUT_SQL
+tar -czf $TAR_SQL ./$OUT_SQL
+rm $OUT_SQL
